b59c972025a809c10e71f590485618fe6a7a1cd7
sarbjit87 <sarbjit1987@gmail.com>
2017-11-24 21:47:38 -0500

    Update pagintator.py

--- pagintator.py
+++ pagintator.py
@@ -20,6 +20,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+# Web2py custom Paginator like Google using Bootstrap
+
 import math
 from gluon import *
 
